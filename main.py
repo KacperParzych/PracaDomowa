b30d9c416358eecc508c53868c9a21f1707e64a5
KacperParzych <157218473+KacperParzych@users.noreply.github.com>
2024-03-09 19:38:31 +0100

    1.0

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
+# 5
+p = 0
+for liczba in range(1, 1000):
+    a = []
+    b = 0
+    for zm in range(1, (liczba//2)+1):
+        if liczba % zm == 0:
+            a.append(zm)
+    for zm2 in range(0, len(a)):
+        b += a[zm2]
+    if liczba == b:
+        p += 1
+print(p)
+# 6
 lista_liczb = [1, 2.5, 3, 4.2, 5]
 for liczba2 in lista_liczb:
     kwadrat = liczba2 ** 2
     print(f"Liczba {liczba2} podniesiona do kwadratu wynosi: {kwadrat}")
+# 7
